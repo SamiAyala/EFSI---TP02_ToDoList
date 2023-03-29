68c039c58ad60e5aaf8c5480831391523f6b93ab
SamiAyala <samirayala23@gmail.com>
2023-03-29 09:02:35 -0300

    avances

--- Script.js
+++ Script.js
@@ -1,21 +1,54 @@
 let ArrayToDos = [
-    {
-        titulo: 'toDo 1',
-        descripcion: 'Hacer algo 1',
-        fechaCreacion: Date.now(),
-        hecho: false
-    },
-    {
-        titulo: 'toDo 2',
-        descripcion: 'Hacer algo 2',
-        fechaCreacion: Date.now(),
-        hecho: false,
-    }
-];
-function mostrarLista(){
-    let listaToDo = document.getElementById("listaToDo");
-    listaToDo.innerHTML="";
-  
-    listaToDo.innerHTML = ArrayToDos.map(toDo => `<li>Titulo: ${toDo.titulo}</li> <li>Descripcion: ${toDo.descripcion}</li>`).join(" - ");
+  {
+    titulo: 'toDo 1',
+    descripcion: 'Hacer algo 1',
+    fechaCreacion: Date.now(),
+    hecho: false
+  },
+  {
+    titulo: 'toDo 2',
+    descripcion: 'Hacer algo 2',
+    fechaCreacion: Date.now(),
+    hecho: false
+  }
+]
+let botonAgregar = document.getElementById('botonAgregar')
+
+botonAgregar.onclick = () => {
+  let inputTitulo = document.getElementById('inputTitulo')
+  let inputDescripcion = document.getElementById('inputDescripcion')
+  if (inputTitulo.value.length>0 || inputDescripcion.value.length>0) {
+    let inputFechaCreacion = Date.now()
+    let inputHecho = false
+    ArrayToDos.push({
+      titulo: inputTitulo.value,
+      descripcion: inputDescripcion.value,
+      fechaCreacion: inputFechaCreacion,
+      hecho: inputHecho
+    })
+    mostrarLista()
+  } else {
+    alert('Alguno de los campos esta vacio.')
+  }
 }
-mostrarLista();
+console.log(ArrayToDos)
+/*botonAgregar.onclick =()=>{
+    let listaToDo =document.getElementById("listaToDo");
+    let inputTitulo=document.getElementById("inputTitulo");
+    let inputDescripcion=document.getElementById("inputDescripcion");
+    let li = document.getElementById("listElement");
+    li.innerHTML = inputTitulo.value;
+    li.innerHTML += inputDescripcion.value;
+
+    listaToDo.appendChild(li);
+}*/
+
+function mostrarLista () {
+  let listaToDo = document.getElementById('listaToDo')
+  listaToDo.innerHTML = ''
+  listaToDo.innerHTML = ArrayToDos.map(
+    toDo =>
+      `<li class="liToDo">Titulo: ${toDo.titulo}<br>Descripcion: ${toDo.descripcion} <br>Fecha de creacion: ${toDo.fechaCreacion}<br>Hecho: ${toDo.hecho}</li>`
+  ).join(' - ')
+}
+mostrarLista()
